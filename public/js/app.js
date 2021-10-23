2e37360be79a78fbd8a3544eef79501277e6131e
ujangaja <ujangaja@gmail.com>
2021-10-23 17:26:41 +0700

    test submit data and view on console

--- public/js/app.js
+++ public/js/app.js
@@ -13,5 +13,21 @@ contactForm.addEventListener('submit', (e) => {
     subject: subject.value,
     message: message.value,
   };
-  console.log(formData);
+  //   console.log(formData);
+  let xhr = new XMLHttpRequest();
+  xhr.open('POST', '/');
+  xhr.setRequestHeader('content-type', 'application/json');
+  xhr.onload = function () {
+    console.log(xhr.responseText);
+    if (xhr.responseText == 'success') {
+      alert('Email sent');
+      name.value = '';
+      email.value = '';
+      subject.value = '';
+      message.value = '';
+    } else {
+      alert('Something went wrong!');
+    }
+  };
+  xhr.send(JSON.stringify(formData));
 });
--- server.js
+++ server.js
@@ -6,9 +6,14 @@ const PORT = process.env.PORT || 5000;
 
 //Middleware
 app.use(express.static('public'));
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/public/contactform.html');
+});
+
+app.post('/', (req, res) => {
+  console.log(req.body);
 });
 
 app.listen(PORT, () => {
