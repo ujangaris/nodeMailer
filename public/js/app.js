5ed98f99e2e9c848c40887b762627073b11cafe4
ujangaja <ujangaja@gmail.com>
2021-10-23 17:11:51 +0700

    formData

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,17 @@
-console.log('hello');
+const contactForm = document.querySelector('.contact-form');
+let name = document.getElementById('name');
+let email = document.getElementById('email');
+let subject = document.getElementById('subject');
+let message = document.getElementById('message');
+
+contactForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  //   console.log('submit clicked');
+  let formData = {
+    name: name.value,
+    email: email.value,
+    subject: subject.value,
+    message: message.value,
+  };
+  console.log(formData);
+});
