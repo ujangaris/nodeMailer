c00e808b3a38a629a4dde917792f2b04dcc7bf72
ujangaja <ujangaja@gmail.com>
2021-10-24 05:14:26 +0700

    send email success

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-
+const nodemailer = require('nodemailer');
 const app = express();
 
 const PORT = process.env.PORT || 5000;
@@ -14,6 +14,28 @@ app.get('/', (req, res) => {
 
 app.post('/', (req, res) => {
   console.log(req.body);
+  const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: 'info.ujangarisandi@gmail.com',
+      pass: 'Admin0k8',
+    },
+  });
+  const mailOptions = {
+    from: req.body.email,
+    to: 'info.ujangarisandi@gmail.com',
+    subject: `Message from ${req.body.email} : ${req.body.subject}`,
+    text: req.body.message,
+  };
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.log(error);
+      res.send('error');
+    } else {
+      console.log('Email sent: ' + info.response);
+      res.send('success');
+    }
+  });
 });
 
 app.listen(PORT, () => {
